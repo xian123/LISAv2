59b0d00773f157b2f8adf0109f164f1702708892
LiliDeng <v-lide@microsoft.com>
2019-07-09 18:36:43 +0800

    Update NET-IFUP-IFDOWN.ps1

--- Testscripts/Windows/NET-IFUP-IFDOWN.ps1
+++ Testscripts/Windows/NET-IFUP-IFDOWN.ps1
@@ -15,49 +15,46 @@ function Main {
         $RootDir
     )
 
-    try {
-        $remoteScript = "NET-IFUP-IFDOWN.sh"
-        $expiration = (Get-Date).AddMinutes(15)
-        Set-Location $RootDir
+    $remoteScript = "NET-IFUP-IFDOWN.sh"
+    $expiration = (Get-Date).AddMinutes(15)
+    Set-Location $RootDir
 
-        #
-        # Run the guest VM side script
-        #
-        $CheckCmd = "echo '${VMPassword}' | sudo -S -s eval `"export HOME=``pwd``;bash ${remoteScript} > NET-IFUP-IFDOWN.log`""
-        Run-LinuxCmd -username $VMUserName -password $VMPassword -ip $Ipv4 -port $VMPort $CheckCmd `
-            -RunInBackground -runAsSudo | Out-Null
-        # Wait for the test to finish running on VM
-        do {
-            if ($TestPlatform -eq "HyperV") {
-                $newIp = Get-IPv4AndWaitForSSHStart -VMName $VMName -HvServer $HvServer `
-                    -VmPort $VmPort -User $VMUserName -Password $VMPassword -StepTimeout 30
-                $allVmData.PublicIP = $newIp
-            }
-            else {
-                $newIp = $allVmData.PublicIP
-            }
-            $state = Run-LinuxCmd -ip $newIp -port $VMPort -username $VMUserName -password $VMPassword "cat state.txt" -ignoreLinuxExitCode:$true
-            Write-LogInfo "Current status:$state"
-            Start-Sleep -Seconds 30
-        } until (($state -eq "TestCompleted") -or ($state -eq "TestAborted") `
-         -or ($state -eq "TestFailed") -or ($state -eq "TestSkipped") -or ((Get-Date) -gt $expiration))
-        Copy-RemoteFiles -download -downloadFrom $newIp -files "/home/${VMUserName}/NET-IFUP-IFDOWN.log" `
-            -downloadTo $LogDir -port $VMPort -username $VMUserName -password $VMPassword
-        if (($state -eq "TestAborted") -or ($state -eq "TestFailed") -or ((Get-Date) -gt $expiration)) {
-            Write-LogErr "Running $remoteScript script failed on VM!"
-            return "FAIL"
-        } elseif ($state -eq "TestSkipped") {
-            Write-LogInfo "Test skipped!"
-            return "SKIPPED"
-        } else {
-            Write-LogInfo "Test passed successfully!"
-            return "PASS"
+    #
+    # Run the guest VM side script
+    #
+    $CheckCmd = "echo '${VMPassword}' | sudo -S -s eval `"export HOME=``pwd``;bash ${remoteScript} > NET-IFUP-IFDOWN.log`""
+    Run-LinuxCmd -username $VMUserName -password $VMPassword -ip $Ipv4 -port $VMPort $CheckCmd `
+        -RunInBackground -runAsSudo | Out-Null
+    # Wait for the test to finish running on VM
+    do {
+        if ($TestPlatform -eq "HyperV") {
+            $newIp = Get-IPv4AndWaitForSSHStart -VMName $VMName -HvServer $HvServer `
+                -VmPort $VmPort -User $VMUserName -Password $VMPassword -StepTimeout 30
+            $allVmData.PublicIP = $newIp
         }
-    } catch {
-        $ErrorMessage = $_.Exception.Message
-        $ErrorLine = $_.InvocationInfo.ScriptLineNumber
-        Write-LogInfo "EXCEPTION: $ErrorMessage at line: $ErrorLine"
-        return "ABORTED"
+        else {
+            $newIp = $allVmData.PublicIP
+        }
+        if (!$newIp) {
+            Start-Sleep -Seconds 30
+            continue
+        }
+        $state = Run-LinuxCmd -ip $newIp -port $VMPort -username $VMUserName -password $VMPassword "cat state.txt" -ignoreLinuxExitCode:$true
+        Write-LogInfo "Current status:$state"
+        Start-Sleep -Seconds 30
+    } until (($state -eq "TestCompleted") -or ($state -eq "TestAborted") `
+     -or ($state -eq "TestFailed") -or ($state -eq "TestSkipped") -or ((Get-Date) -gt $expiration))
+    Copy-RemoteFiles -download -downloadFrom $newIp -files "/home/${VMUserName}/NET-IFUP-IFDOWN.log" `
+        -downloadTo $LogDir -port $VMPort -username $VMUserName -password $VMPassword
+    if (($state -eq "TestAborted") -or ($state -eq "TestFailed") -or ((Get-Date) -gt $expiration)) {
+        Write-LogErr "Running $remoteScript script failed on VM!"
+        return "FAIL"
+    } elseif ($state -eq "TestSkipped") {
+        Write-LogInfo "Test skipped!"
+        return "SKIPPED"
+    } else {
+        Write-LogInfo "Test passed successfully!"
+        return "PASS"
     }
 }
 Main -VMName $AllVMData.RoleName -HvServer $TestLocation `
